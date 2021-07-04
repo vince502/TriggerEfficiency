e03602cfdf2dd7d516ff2af08545c36737ddb81c
Gabriel <luisg.gallegosm@outlook.com>
2021-07-04 16:38:13 -0700

    Adjusted centrality bins.

--- PlotEff/PlotEff.h
+++ PlotEff/PlotEff.h
@@ -42,8 +42,12 @@ const std::vector<double> ptBinsHigh=[]()
     return out;
 }();
 const std::vector<double> yBins { -2.4, -2.0,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.0,2.4 };
-const std::vector<double> centBins { 194.0,195.0,196.0,197.0,198.0,199.0,200.0};
-
+const std::vector<double> centBins=[]()
+{
+    std::vector<double> out;
+    for(int i=0;i<200;i+=10) out.push_back(i);
+    return out;
+}();
 const char histPtLabel[]="p_{t}^{#mu#mu}(GeV/c)";
 const char histYLabel[]="y^{#mu#mu}";
 const char histCentLabel[]="Centrality";
