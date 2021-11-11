1cc2c8bb4a6468256d8f772e0b54379e13425090
soohwan <soohwan.lee@cern.ch>
2021-11-11 03:01:47 +0100

    Add branch for reco QQ

--- OniaIO/Data.cpp
+++ OniaIO/Data.cpp
@@ -35,6 +35,7 @@ OniaInput::OniaInput()
 {
     reco_mu_mom4 = new TClonesArray("TLorentzVector");
     reco_mu_L1_mom4 = new TClonesArray("TLorentzVector");
+    reco_QQ_mom4 = new TClonesArray("TLorentzVector");
 }
 
 void OniaInput::registerInput(TreeReader* reader)
@@ -44,18 +45,24 @@ void OniaInput::registerInput(TreeReader* reader)
     reader->addInput("Centrality",&centrality);
 
     reader->addInput("Reco_mu_size", &reco_mu_size);
+    reader->addInput("Reco_QQ_size", &reco_QQ_size);
     reader->addInput("Reco_mu_nTrkWMea",&reco_mu_nTrkWMea);
     reader->addInput("Reco_mu_nPixWMea",&reco_mu_nPixWMea);
     reader->addInput("Reco_mu_dxy",&reco_mu_dxy);
     reader->addInput("Reco_mu_dz",&reco_mu_dz);
     reader->addInput("Reco_mu_4mom",&(reco_mu_mom4));
     reader->addInput("Reco_mu_L1_4mom", &(reco_mu_L1_mom4));
+    reader->addInput("Reco_QQ_4mom",&(reco_QQ_mom4));
+    reader->addInput("Reco_QQ_mupl_idx",&reco_QQ_mupl_idx);
+    reader->addInput("Reco_QQ_mumi_idx",&reco_QQ_mumi_idx);
+
 }
 
 OniaInput::~OniaInput()
 {
     delete reco_mu_mom4;
     delete reco_mu_L1_mom4;
+    delete reco_QQ_mom4;
 }
 
 void OniaOutput::registerOutput(TreeWriter* writer)
